7e4affb1d24c99ee358ae62f57ba6293756f44ce
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-10-23 15:21:19 -0700

    Finish first routes project in rails

---
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    # puts user_params
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  rescue ActiveRecord::RecordNotFound
+    render plain: "user does not exist", status: :unprocessable_entity
+  end
+
+  def update # (youll want to use ActiveRecord::Base#update)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render plain: "user does not exist", status: :unprocessable_entity
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render plain: "user does not exist", status: :unprocessable_entity
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
